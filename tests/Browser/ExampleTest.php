1b2def5a42f5e9c93509bb9d00418537d870dd20
Andrew Peroramas <wolfandrew307@gmail.com>
2025-05-13 04:29:18 +0800

    add dusk

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+final class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     */
+    public function test_basic_example(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                ->screenshot('homepage')
+                ->assertSee('Sassykit');
+        });
+    }
+}
