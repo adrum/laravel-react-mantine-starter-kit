ec3cb189afbe5f81087ae72a536188b390d0608c
Andrew Peroramas <wolfandrew307@gmail.com>
2025-05-13 01:11:24 +0800

    add filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,10 +1,8 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\SocialiteUiServiceProvider::class,
     SocialiteUi\SocialiteUiServiceProvider::class,
-
 ];
