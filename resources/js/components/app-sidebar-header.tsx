e5f80e4adc66595ec188a4f562ea2852a471cc29
Austin Drummond <adrum0x7@gmail.com>
2026-03-14 11:15:08 -0400

    fix header border when sidebar

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@mantine/core';
 import { IconLayoutSidebar } from '@tabler/icons-react';
 import { Breadcrumbs } from '@/components/breadcrumbs';
-import type {BreadcrumbItem} from '@/types';
+import type { BreadcrumbItem } from '@/types';
 
 type Props = {
     breadcrumbs?: BreadcrumbItem[];
@@ -10,7 +10,7 @@ type Props = {
 
 export function AppSidebarHeader({ breadcrumbs = [], toggle }: Props) {
     return (
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b border-sidebar-border/50 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
+        <header className="flex h-full shrink-0 items-center gap-2 px-6 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 md:px-4">
             <Button
                 variant="subtle"
                 className="p-0"
